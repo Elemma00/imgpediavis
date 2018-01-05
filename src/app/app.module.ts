34e9cfee3bf45ef8238f4a311be33690bd160054
Nicolás Bravo <nicolasbravo94@gmail.com>
2018-01-05 12:25:59 -0300

    image detail component created and added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { ImgDetailComponent } from './img-detail/img-detail.component';
+
+
+const routes: Routes = [
+  {path: 'detail', component: ImgDetailComponent},
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
