1a79b5f4ab3953e62f135b110165d020db3f5b26
Nicolás Bravo <nicolasbravo94@gmail.com>
2018-01-09 02:33:02 -0300

    added angular-material base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {Routes, RouterModule} from '@angular/router';
 
+/* Material */
+import { MatCardModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material';
 
+/* App components */
 import { AppComponent } from './app.component';
 import { ImgDetailComponent } from './img-detail/img-detail.component';
 
+/* App services */
 import {MainService} from './main.service';
 
 const routes: Routes = [
@@ -21,8 +28,12 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
     HttpClientModule,
-    RouterModule.forRoot(routes, {useHash: true})
+    RouterModule.forRoot(routes, {useHash: true}),
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [MainService],
   bootstrap: [AppComponent]
